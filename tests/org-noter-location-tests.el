018665125b197339b435a62c522cbaaed0fd26e6
Dmitry M <dmitrym@gmail.com>
2023-02-21 19:15:37 -0800

    Ensure that goto-location hook is called with expected variables.

--- tests/org-noter-location-tests.el
+++ tests/org-noter-location-tests.el
@@ -17,7 +17,7 @@
 :END:
 ** Heading2
 :PROPERTIES:
-:NOTER_PAGE: (41 0.09 . 0.16)
+:NOTER_PAGE: (699 . 0.123031)
 :HIGHLIGHT: #s(pdf-highlight 41 ((0.18050847457627117 0.09406231628453851 0.6957627118644067 0.12110523221634333)))
 :END:
 #+BEGIN_QUOTE
@@ -43,7 +43,7 @@ Test
                                       (org-noter-core-test-create-session)
                                       (search-forward "Heading2")
                                       (expect (org-noter--get-containing-heading) :not :to-be nil)
-                                      (expect (org-noter--parse-location-property (org-noter--get-containing-element)) :to-equal (read "(41 0.09 . 0.16)"))
+                                      (expect (org-noter--parse-location-property (org-noter--get-containing-element)) :to-equal (read "(699 . 0.123031)"))
                                       )
 
                                    ))
@@ -51,6 +51,7 @@ Test
                               )
 
                     )
+
 
           ;; can't have proper nav without sessions
           (describe "session"
@@ -68,19 +69,18 @@ Test
                                    )))
 
                     )
+
           (describe "navigation functions"
                     (before-each
                      ;; our location handling
                      (add-to-list 'org-noter--doc-goto-location-hook #'org-noter-location-test-goto-location)
                      (spy-on 'org-noter-location-test-goto-location :and-return-value t)
 
-                     ;; not setting the session correctly ¯\_(ツ)_/¯
                      (spy-on 'org-noter--get-or-read-document-property :and-return-value "pubs/solove-nothing-to-hide.pdf")
 
 
                      ;; currently not setting the session correctly. set the _test_window inside the test
                      (spy-on 'org-noter--get-notes-window :and-call-fake (lambda ()
-
                                                                            _test_window))
                      )
 
@@ -100,8 +100,7 @@ Test
                              )
                             (org-noter-sync-current-note)
                             (expect 'org-noter-location-test-goto-location :to-have-been-called)
-                            ;; TODO This will fail because the session is not setup correctly.
-                            ;; (expect 'org-noter-location-test-goto-location :to-have-been-called-with "mode" "location")
+                            (expect 'org-noter-location-test-goto-location :to-have-been-called-with "pubs/solove-nothing-to-hide.pdf"  (read "(699 . 0.123031)") _test_window)
                             )
                          ))
                     )
